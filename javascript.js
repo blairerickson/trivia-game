e3a8e933410423dfdbf996ceecd8293b033bf768
Blair Erickson <blair@jamwix.com>
2017-02-24 02:35:44 -0500

    randomize trivia movie gifs

--- javascript.js
+++ javascript.js
@@ -4,18 +4,35 @@
 
 var images = ["http://lorempixel.com/600/400/city/", "http://lorempixel.com/600/400/city/", "http://lorempixel.com/600/400/people/"];
 
-var movies = ["Schindler's List", "Halloween 2","Naked Gun", "Deep Blue Sea", "Drag Me to Hell","Rambo","Kidergarten Cop","Total Recall","The Thing", "Tango and Cash","The Expendables","2 Fast 2 Furious","Die Hard 2","Police Academy","Die Hard 3","Die Hard","Speed", "Friday the 13th part 2","Empire of the Sun","Gravity", "Rebel Without a Cause","Enemy At the Gates","Saving Private Ryan","Sunset Boulevard", "Creature From the Black Lagoon","Lake Placid","Dumb and Dumber","Me Myself and Irene","Eternal Sunshine of the Spotless Mind","Jurassic Park", "The Big Lebowski", "Banshee Chapter","E.T.", "Gremlins","Deadpool","Terminator","Terminator 2","King Kong","A Nightmare on Elm Street","The Avengers","Children of Men", "Oldboy","Steel Magnolias","Can't Hardly Wait","The Breakfast Club"];
+var movies = ["Ferris Bueller's Day Off","What Dreams May Come","Fear and Loathing in Las Vegas","Cabin in the Woods","Schindler's List", "Halloween 2","Naked Gun", "Deep Blue Sea", "Drag Me to Hell","Rambo","Kindergarden Cop","Total Recall","The Thing", "Tango and Cash","The Expendables","2 Fast 2 Furious","Die Hard 2","Police Academy","Die Hard 3","Die Hard","Speed", "Friday the 13th part 2","Empire of the Sun","Gravity", "Rebel Without a Cause","Enemy At the Gates","Saving Private Ryan","Sunset Boulevard", "Creature From the Black Lagoon","Lake Placid","Dumb and Dumber","Me Myself and Irene","Eternal Sunshine of the Spotless Mind","Jurassic Park", "The Big Lebowski", "Banshee Chapter","E.T.", "Gremlins","Deadpool","Terminator","Terminator 2","King Kong","A Nightmare on Elm Street","The Avengers","Children of Men", "Oldboy","Steel Magnolias","Can't Hardly Wait","The Breakfast Club"];
 
 var random = Math.floor((Math.random() * movies.length) + 1);
+var dice = Math.floor((Math.random() * movies.length) + 1);
+
 
 var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + movies[random]+ '&limit=1&api_key=dc6zaTOxFJmzC';
 
 
-var Qrand = Math.floor((Math.random() * 3) + 1);
+var Qwrong = Math.floor((Math.random() * movies.length) + 1);
 var Arand = Math.floor((Math.random() * 3) + 1);
 
 
+function AnswerSort()
+{
+ for (var i = 1; i < 5; i++)
+ {
+ if (Arand == i)
+      {
+        $('#Ans' + i).html(movies[random]);
+      }
+ else
+    {
+    dice = Math.floor((Math.random() * movies.length) + 1);
+        $('#Ans' + i).html(movies[dice]);
+    }
 
+ }
+}
 
 var showImage;
 
@@ -23,7 +40,8 @@ var showImage;
 var count = 0;
 
 $("#begin").click(startSlideshow);
-$("#stop").click(stopSlideshow);
+$("#begin").click(AnswerSort);
+
 
 
 $("#questionbox").html("TEST QUESTION FOR QUESTION BOX.");
@@ -33,7 +51,9 @@ $("#questionbox").html("TEST QUESTION FOR QUESTION BOX.");
 // This function will replace display whatever image it's given
 // in the 'src' attribute of the img tag.
 function displayImage() {
-    
+
+var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + movies[random]+ '&limit=1&api_key=dc6zaTOxFJmzC';
+
 
   $.ajax({
       url: queryURL,
@@ -53,17 +73,14 @@ function displayImage() {
 function nextImage() {
   //  TODO: Increment the count by 1.
   count++;
+  random = Math.floor((Math.random() * movies.length) + 1);
+  AnswerSort();
 
-  // TODO: Show the loading gif in the "image-holder" div.
-  $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
+ $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
 
-  // TODO: Use a setTimeout to run displayImage after 5 seconds.
+  
   setTimeout(displayImage, 1000);
 
-  // TODO: If the count is the same as the length of the image array, reset the count to 0.
-  if (count === images.length) {
-    count = 0;
-  }
 }
 
 function startSlideshow() {
